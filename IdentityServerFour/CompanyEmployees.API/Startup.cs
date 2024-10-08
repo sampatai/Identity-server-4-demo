ca6cf14d96539ae7b5cbfe257635ca10d38356bb
SaMpat Adhikari <sampat.adhikari@gmail.com>
2024-10-08 08:13:55 +0545

    add identity server four project demo

---
+++ IdentityServerFour/CompanyEmployees.API/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CompanyEmployees.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", opt =>
+                {
+                    opt.RequireHttpsMetadata = false;//RequireHttpsMetadata – We set this to false because we are in a development environment and we don’t require HTTPS
+                    opt.Authority = "https://localhost:5005";//Address to use when sending OpenID Connect calls
+                    opt.Audience = "omgApi";//– Audience value for received OpenID Connect tokens. This value has to be the same as the one provided in the authorization server configuration for the API resource
+                });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ IdentityServerFour/CompanyEmployees.OAuth/CompanyEmployees.OAuth/Configuration/InMemoryConfig.cs
@@ -0,0 +1,72 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace CompanyEmployees.OAuth.Configuration
+{
+    /// <summary>
+    /// This class will consist of different configurations related to Users, Clients, IdentityResources, etc
+    /// . So, let’s add them one by one
+    /// </summary>
+    public static class InMemoryConfig
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+             new List<IdentityResource>
+             {
+                 new IdentityResources.OpenId(),//With the OpenId method, we support a subject id or sub value to be included
+                 new IdentityResources.Profile()//to support profile information like given_name or family_name.
+             };
+
+        public static List<TestUser> GetUsers() => new List<TestUser>
+        {
+              new TestUser
+      {
+          SubjectId = "a9ea0f25-b964-409f-bcce-c923266249b4",
+          Username = "papi",
+          Password = "papiPassword",
+          Claims = new List<Claim>
+          {
+              new Claim("given_name", "papi"),
+              new Claim("family_name", "lama")
+          }
+      },
+      new TestUser
+      {
+          SubjectId = "c95ddb8c-79ec-488a-a485-fe57a1462340",// SubjectId supported by the OpenId IdentityResource
+          Username = "sweetRani",
+          Password = "sweetPassword",
+          Claims = new List<Claim>
+          {
+              new Claim("given_name", "sweet"),// given_name and family_name claims supported by the Profile IdentityResource
+              new Claim("family_name", "shah")
+          }
+      }
+        };
+
+        public static IEnumerable<Client> GetClients() => new List<Client>
+        {
+            new Client
+            {
+                ClientId="omg-employee",
+                ClientSecrets=new []{new Secret("omgsecret".Sha512())},
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,//AllowedGrantTypes provides the information about the flow
+                                                                                       //we are going to use to get the token
+                AllowedScopes={ IdentityServerConstants.StandardScopes.OpenId, "omgApi" } //allows us to trade user credentials for the token
+
+            }
+        };
+        public static IEnumerable<ApiScope> GetApiScopes() => new List<ApiScope> { new ApiScope("omgApi", "CompanyEmployee API") };
+
+        public static IEnumerable<ApiResource> GetApiResources() =>
+             new List<ApiResource>
+            {
+              new ApiResource("omgApi", "CompanyEmployee API")
+            {
+                Scopes = { "omgApi" }
+            }
+       };
+    }
+}
